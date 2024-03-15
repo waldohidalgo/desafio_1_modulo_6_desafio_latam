eeb0fa765b8a4c5210ef5f5bde096ade33864778
Waldo Hidalgo <whidalgohp@gmail.com>
2024-03-15 16:44:27 -0300

    agrego href con js en el html inicio

---
+++ inicio/script.js
@@ -0,0 +1,4 @@
+/* Agrego href a anchor tag para linkear a rutas*/
+const rutaUsuarioHref = document.getElementById("ruta_usuarios");
+
+rutaUsuarioHref.href = location.origin + "/abracadabra/usuarios";
