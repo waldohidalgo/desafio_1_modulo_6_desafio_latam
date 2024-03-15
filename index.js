2651dcca28d559d9bfd251ceb80d0d9f573ee40e
Waldo Hidalgo <whidalgohp@gmail.com>
2024-03-15 13:24:55 -0300

    update port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en el puerto: ${port}`);
